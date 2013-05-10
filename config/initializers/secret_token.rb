7913088d3a1410e30499545318d79e2a9a9e0728
Handle <Handle.huang0409@gamil.com>
2013-05-10 10:37:28 +0800

    ab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tulip::Application.config.secret_token = '142cb3b754adc63689d68d13b1649be3dd88a5b13b3cb02f4e1bd7b8c3edf0d7a3e035a4b0458e73d225bf435885c4b284b07458ab02a9cd594db69457bdb459'
